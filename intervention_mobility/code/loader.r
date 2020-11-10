b3c8f96c9f735b4145127d43378c782a51231ee4
Kenneth Lee <leechinhongkenneth@gmail.com>
2020-11-09 17:40:01 -0800

    fix bugs and add more helper functions

---
+++ intervention_mobility/code/loader.r
@@ -0,0 +1,197 @@
+load_covidcast_data <- function(STARTDATE, ENDDATE, GEO_TYPE, GEO_VALUE, EXCLUDED_AREAS){
+  
+  # full-time away from home signal
+  ftime <- covidcast_signal(data_source = "safegraph", 
+                            signal ="full_time_work_prop",
+                            start_day = STARTDATE, 
+                            end_day = ENDDATE,
+                            geo_type = GEO_TYPE, 
+                            geo_values = GEO_VALUE)
+  
+  ftime <- ftime %>%  
+    filter(!(geo_value %in% EXCLUDED_AREAS))
+  
+  # Part time away from home signal
+  ptime <- covidcast_signal(data_source = "safegraph", 
+                            signal ="part_time_work_prop",
+                            start_day = STARTDATE, 
+                            end_day = ENDDATE,
+                            geo_type = GEO_TYPE, 
+                            geo_values = GEO_VALUE)
+  
+  ptime <- ptime %>%  
+    filter(!(geo_value %in% EXCLUDED_AREAS))
+  
+  # The fraction of mobile devices that 
+  # did not leave the immediate area of their home
+  completely_home_prop <- covidcast_signal(data_source = "safegraph", 
+                            signal ="completely_home_prop",
+                            start_day = STARTDATE, 
+                            end_day = ENDDATE,
+                            geo_type = GEO_TYPE, 
+                            geo_values = GEO_VALUE)
+  
+  completely_home_prop <- completely_home_prop %>%  
+    filter(!(geo_value %in% EXCLUDED_AREAS))
+  
+  
+  # The median time spent at home for all devices at this location 
+  # for this time period, in minutes
+  median_home_dwell_time <- covidcast_signal(data_source = "safegraph", 
+                                             signal ="median_home_dwell_time",
+                                             start_day = STARTDATE, 
+                                             end_day = ENDDATE,
+                                             geo_type = GEO_TYPE, 
+                                             geo_values = GEO_VALUE)
+  
+  median_home_dwell_time <- median_home_dwell_time %>%  
+    filter(!(geo_value %in% EXCLUDED_AREAS))
+  
+  ############## New confirmed COVID19 cases ############
+  
+  # A composite signal from JHU and USA facts
+  # New confirmed COVID19 cases on average per 7 days
+  case_avg <- covidcast_signal(data_source = "indicator-combination",
+                               signal ="confirmed_7dav_incidence_num",
+                               start_day = STARTDATE, 
+                               end_day = ENDDATE,
+                               geo_type = GEO_TYPE, 
+                               geo_values = GEO_VALUE)
+  
+  case_avg <- case_avg %>%  filter(!(geo_value %in% EXCLUDED_AREAS))
+  
+  # Cumulative confirmed COVID19 cases on average per 7 days
+  cum_case <- covidcast_signal(data_source = "indicator-combination",
+                               signal = "confirmed_7dav_cumulative_num",
+                               start_day = STARTDATE,
+                               end_day = ENDDATE,
+                               geo_type = GEO_TYPE,
+                               geo_values = GEO_VALUE)
+  
+  cum_case <- cum_case %>%  filter(!(geo_value %in% EXCLUDED_AREAS))
+  
+  # Cumulative confirmed COVID19 cases on average per 7 days, per 100,000 population
+  
+  cum_case_prop <- covidcast_signal(data_source = "indicator-combination",
+                                    signal ="confirmed_7dav_cumulative_prop",
+                                    start_day = STARTDATE, 
+                                    end_day = ENDDATE,
+                                    geo_type = GEO_TYPE, 
+                                    geo_values = GEO_VALUE)
+  
+  cum_case_prop <- cum_case_prop %>%  filter(!(geo_value %in% EXCLUDED_AREAS))
+  
+  ########### Death cases ######################
+  
+  # Number of new confirmed deaths due to COVID-19, daily
+  death_case <- covidcast_signal(data_source = "indicator-combination",
+                                 signal ="deaths_7dav_incidence_num",
+                                 start_day = STARTDATE, 
+                                 end_day = ENDDATE,
+                                 geo_type = GEO_TYPE, 
+                                 geo_values = GEO_VALUE)
+  
+  death_case <- death_case %>%  filter(!(geo_value %in% EXCLUDED_AREAS))
+  
+  # Cumulative number of confirmed deaths due to COVID-19
+  cum_death_case <- covidcast_signal(data_source = "indicator-combination",
+                                     signal ="deaths_7dav_cumulative_num",
+                                     start_day = STARTDATE, 
+                                     end_day = ENDDATE,
+                                     geo_type = GEO_TYPE, 
+                                     geo_values = GEO_VALUE)
+  cum_death_case  <- cum_death_case  %>%  filter(!(geo_value %in% EXCLUDED_AREAS))
+  
+  # doctor-visits with systematic day-of-week effects
+  smoothed_cli <-covidcast_signal(data_source = "doctor-visits",
+                                  signal ="smoothed_cli",
+                                  start_day = STARTDATE, 
+                                  end_day = ENDDATE,
+                                  geo_type = GEO_TYPE, 
+                                  geo_values = GEO_VALUE)
+  
+  smoothed_cli  <- smoothed_cli %>% filter(!(geo_value %in% EXCLUDED_AREAS))
+  
+  # with systematic day-of-week effects
+  smoothed_adj_cli <- covidcast_signal(data_source = "doctor-visits",
+                                    signal ="smoothed_adj_cli",
+                                    start_day = STARTDATE, 
+                                    end_day = ENDDATE,
+                                    geo_type = GEO_TYPE, 
+                                    geo_values = GEO_VALUE)
+  
+  
+  # State-level restaurant data
+  res <- read.csv("data/state_restaurants_visit_num.csv")
+  # Change date type
+  res$date <- ymd(res$date)
+  # Filter the data
+  res <- res %>% filter(!(geo_id %in% EXCLUDED_AREAS) & (date >= STARTDATE & date <= ENDDATE))
+  # drop some columns
+  res <- res[c(1,2,4,5)]
+  # Change column names
+  colnames(res) <- c("geo_value", "value", "sample_size", "time_value")
+  
+  new_res <- res %>% transmute (
+    geo_value = geo_value,
+    signal = "state_restaurant_visit_num",
+    time_value = time_value,
+    direction = NA,
+    issue = lubridate::today(),
+    lag = issue - time_value,
+    value = value,
+    stderr = NA,
+    sample_size = NA,
+    data_source = 'Weekly Patterns')
+  return(list(Full.Time.Mobility=ftime, 
+              Part.Time.Mobility=ptime,
+              completely_home_prop=completely_home_prop,
+              median_home_dwell_time = median_home_dwell_time,
+              Avg.Confirmed.Case.Count=case_avg,  
+              Cum.Avg.Case.Count=cum_case, 
+              Cum.Avg.Case.Count.Prop = cum_case_prop, 
+              Avg.Death.Case.Count = death_case, 
+              Cum.Avg.Death.Count = cum_death_case, 
+              Restaurant.Visit.Count = new_res, 
+              smoothed_cli = smoothed_cli,   
+              smoothed_adj_cli= smoothed_adj_cli))
+}
+
+
+load_policy <- function(){
+  # Read government intervention data
+  urlfile="https://raw.githubusercontent.com/COVID19StatePolicy/SocialDistancing/master/data/USstatesCov19distancingpolicy.csv"
+  policy <- read_csv(url(urlfile))
+  
+  # Remove data that contains NA for every column
+  idx <- apply(policy, 1, function(x) all(is.na(x)))
+  # Override the loaded policy data
+  policy <- policy[!idx, ]
+  
+  
+  # Convert to lower case
+  policy$StatePostal <- tolower(policy$StatePostal)
+  
+  # First we convert the date to a proper format
+  dated.policy <- lapply(policy[, c("DateIssued",
+                    "DateEnacted", 
+                    "DateExpiry" ,
+                    "DateEased", 
+                    "DateEnded", 
+                    "DateReexpanded1", 
+                    "DateReeased1")], 
+         function(x) as.Date(as.character(x), 
+                             "%Y%m%d"))
+  
+  policy[, c("DateIssued", 
+             "DateEnacted", 
+             "DateExpiry" ,
+             "DateEased", 
+             "DateEnded", 
+             "DateReexpanded1", 
+             "DateReeased1")] <- data.frame(dated.policy)
+  
+  return(policy)
+}
+
+
--- intervention_mobility/code/painter.r
+++ intervention_mobility/code/painter.r
@@ -4,45 +4,16 @@ library(tidyverse)
 library(dplyr)
 library(covidcast)
 
-# A function to compute the corrlation by various number of shifts
-getCorrByShift <- function(num_shift, signal_1, signal_2, corr_method, by){
-  # num_shift: specify how many days to forward signal_1
-  # signal_1: the first signal
-  # signal_2: the second signal
-  # corr_method: correlation method
-  # by: select either time_value or geo_value
-  dt_vec <- 0:num_shift
-  df_list <- vector("list", length(dt_vec))
-  for (i in 1:length(dt_vec)) {
-    df_list[[i]] <- covidcast_cor(signal_1, signal_2, dt_x = dt_vec[i], by = by, method= corr_method)
-    df_list[[i]]$dt <- dt_vec[i]
-  }
-  df <- do.call(rbind, df_list)
-  return(df)
-}
+########## plot.all.Corr.Median.by.shift() ################
 
-# A function to derive median of the signal from each shift 
-getMedian <-function(df){
-  df_med <- df %>%
-    group_by(dt) %>%
-    summarize(median = median(value, na.rm = TRUE), .groups = "drop_last")
-  return(df_med)
-}
-
-getMedianCorr <- function(shiftday, covidcast.like_signal.1, covidcast.like_signal.2, corr.method, name, by){
-  # shiftday: specify the number of days you want to shift
-  # covidcast.like_signal.1: a dataframe that has covidcast package ouput dataframe 
-  # covidcast.like_signal.2: another dataframe that has covidcast package ouput dataframe 
-  # corr.method: specify a correlation method
-  # name: specify the name of the signal
+plot.all.Corr.Median.by.shift <- function(other_signals, 
+                                          main_signal, 
+                                          shiftday, 
+                                          names, 
+                                          corr.method, 
+                                          title, 
+                                          by_method){
   
-  df <- getCorrByShift(shiftday, covidcast.like_signal.1, covidcast.like_signal.2, corr.method, by)
-  med <- getMedian(df)
-  med$Comparison <- name
-  return(med)
-}
-
-plot.all.Corr.Median.by.shift <- function(other_signals, main_signal, shiftday, names, corr.method, title, by_method){
   # Compute correlation between other covidcast-like signals and mobility
   df.ls = list()
   for (i in 1:length(other_signals)){
@@ -61,48 +32,33 @@ plot.all.Corr.Median.by.shift <- function(other_signals, main_signal, shiftday, 
 }
 
 
-# Multiple plot function
-#
-# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
-# - cols:   Number of columns in layout
-# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
-#
-# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
-# then plot 1 will go in the upper left, 2 will go in the upper right, and
-# 3 will go all the way across the bottom.
-#
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-  library(grid)
+########## plotInterventionTime() ################
+# Plot the first day of the intervention over time via ggplot
+# arguments:
+
+# intervention_mobility_case (data.frame): a dataframe that contains mobility
+# left joined by specified policy dataframe
+
+# mobility.name (character) : 
+# intervention.first.day(character): a string that specify the first date of 
+#                                   intervention
+
+# ylab (character): name for y axis 
+# xlab (character): name for x axis
+
+plotInterventionTime <-function(intervention_mobility_case, 
+                                mobility.name,
+                                intervention.first.day,
+                                ylab,
+                                xlab){
   
-  # Make a list from the ... arguments and plotlist
-  plots <- c(list(...), plotlist)
+  # Plot the time-series: mobility signal as y, time as x 
+  intervention_mobility_case %>%
+    ggplot(aes_string(x = "time_value", y = mobility.name)) + 
+    geom_point() +
+    geom_vline(xintercept = intervention.first.day) + 
+    labs(y = "Full time away home signal", 
+         x = "time-value")
   
-  numPlots = length(plots)
-  
-  # If layout is NULL, then use 'cols' to determine layout
-  if (is.null(layout)) {
-    # Make the panel
-    # ncol: Number of columns of plots
-    # nrow: Number of rows needed, calculated from # of cols
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-                     ncol = cols, nrow = ceiling(numPlots/cols))
-  }
-  
-  if (numPlots==1) {
-    print(plots[[1]])
-    
-  } else {
-    # Set up the page
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-    
-    # Make each plot, in the correct location
-    for (i in 1:numPlots) {
-      # Get the i,j matrix positions of the regions that contain this subplot
-      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-      
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-                                      layout.pos.col = matchidx$col))
-    }
-  }
 }
+
